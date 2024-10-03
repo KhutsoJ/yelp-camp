e2082f0ee79a1bdf4527a3685804afc69d11499f
Khutso <khutsoj1@gmail.com>
2024-10-03 21:17:58 +0200

    added delete and edit for campgrounds, full CRUD functionality

--- app.js
+++ app.js
@@ -2,12 +2,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
+const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 
 const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 
 mongoose.connect('mongodb://127.0.0.1:27017/yelpCamp')
    .then(() => {
@@ -39,11 +41,10 @@ app.get('/campgrounds', async (req, res) => {
 
 //GET "MAKE NEW CAMPGROUND" FORM
 app.get('/campgrounds/create', (req, res) => {
-   res.render("campgrounds/create");
+   res.render('campgrounds/create');
 })
-
 //POST NEW CAMPGROUND
-app.post('/campgrounds/:id', async (req, res) => {
+app.post('/campgrounds', async (req, res) => {
    const campground = new Campground(req.body.campground);
    await campground.save();
    res.redirect(`/campgrounds/${campground._id}`);
@@ -53,4 +54,24 @@ app.post('/campgrounds/:id', async (req, res) => {
 app.get('/campgrounds/:id', async (req, res) => {
    const campground = await Campground.findById(req.params.id);
    res.render('campgrounds/show', { campground });
+})
+
+//GET EDIT FORM FOR CAMPGROUND
+app.get('/campgrounds/:id/edit', async (req, res) => {
+   const { id } = req.params;
+   const campground = await Campground.findById(id);
+   res.render('campgrounds/edit', { campground });
+})
+//UPDATE THE CAMPGROUND
+app.put('/campgrounds/:id', async (req, res) => {
+   const { id } = req.params;
+   const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground }, { new: true });
+   res.redirect(`/campgrounds/${id}`);
+})
+
+//DELETE A CAMPGROUND
+app.delete('/campgrounds/:id', async (req, res) => {
+   const { id } = req.params;
+   const campground = await Campground.findByIdAndDelete(id);
+   res.redirect(`/campgrounds`);
 })
