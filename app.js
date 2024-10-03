a838981ccaa9e939bfbaf93389449b0ddfd5166b
Khutso <khutsoj1@gmail.com>
2024-10-03 14:44:46 +0200

    added post functionality for new campgrounds

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const path = require('path');
 const Campground = require('./models/campground');
 
 const app = express();
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded({ extended: true }));
 
 mongoose.connect('mongodb://127.0.0.1:27017/yelpCamp')
    .then(() => {
@@ -14,8 +17,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/yelpCamp')
       console.log("ERROR:" + err.message);
    })
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+
 
 app.listen(3000, () => {
    console.log("LISTENING ON PORT 3000");
@@ -29,7 +31,26 @@ app.get('/', (req, res) => {
    res.render('home')
 })
 
+//GET CAMPGROUNDS INDEX
 app.get('/campgrounds', async (req, res) => {
    const campgrounds = await Campground.find();
    res.render('campgrounds/index', { campgrounds });
+})
+
+//GET "MAKE NEW CAMPGROUND" FORM
+app.get('/campgrounds/create', (req, res) => {
+   res.render("campgrounds/create");
+})
+
+//POST NEW CAMPGROUND
+app.post('/campgrounds/:id', async (req, res) => {
+   const campground = new Campground(req.body.campground);
+   await campground.save();
+   res.redirect(`/campgrounds/${campground._id}`);
+})
+
+//GET CAMPGROUND DETAILS
+app.get('/campgrounds/:id', async (req, res) => {
+   const campground = await Campground.findById(req.params.id);
+   res.render('campgrounds/show', { campground });
 })
