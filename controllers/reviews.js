1ec9da1e34f1cef542f12f088e12a62f6870e073
Khutso <khutsoj1@gmail.com>
2024-10-16 21:29:53 +0200

    more refactoring of routes

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -5,7 +5,7 @@ module.exports.createReview = async (req, res) => {
    const campground = await Campground.findById(req.params.id);
    const review = await new Review(req.body.review);
    review.author = req.user._id;
-   campground.reviews.push(review);
+   campground.reviews.unshift(review);
    await review.save();
    await campground.save();
    req.flash('success', 'Review successfully added');
